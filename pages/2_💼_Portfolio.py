3adb31bc406d2419e3060db51236e40393fa1be4
charlesbear09-crypto <charlesbear09@gmail.com>
2026-08-01 21:58:03 -0700

    Update 2_💼_Portfolio.py

--- pages/2_💼_Portfolio.py
+++ pages/2_💼_Portfolio.py
@@ -1,72 +1,66 @@
 import streamlit as st
 
+from auth import unlock_app
 from trading_engine import get_portfolio
+
+
+
+if not unlock_app():
+    st.stop()
+
 
 
 st.title("💼 Athena Portfolio")
 
 
+
 portfolio = get_portfolio()
 
 
-# Cash
+
 st.metric(
     "💵 Fake Cash",
     f"${portfolio['cash']:,.2f}"
 )
 
 
+
 st.divider()
 
 
-# Holdings
-st.subheader("📊 Owned Stocks")
+
+st.subheader("📊 Holdings")
+
 
 
 if portfolio["stocks"]:
 
-    for symbol, data in portfolio["stocks"].items():
+    for stock, data in portfolio["stocks"].items():
 
         st.write(
             f"""
-            ### {symbol}
+            ## {stock}
 
             Shares: {data['shares']}
 
-            Average Buy Price: ${data['average_price']}
-
+            Average Price: ${data['average_price']}
             """
         )
 
 else:
 
-    st.info("No stocks owned yet.")
+    st.info("No stocks owned")
 
 
 
 st.divider()
 
 
-# Trade history
+
 st.subheader("📜 Trade History")
 
 
-if portfolio["history"]:
 
-    for trade in portfolio["history"]:
+for trade in portfolio["history"]:
 
-        st.write(
-            f"""
-            **{trade['action']}**
-
-            Stock: {trade['stock']}
-
-            Shares: {trade['shares']}
-
-            Price: ${trade['price']}
-            """
-        )
-
-else:
-
-    st.write("No trades yet.")
+    st.write(trade)
